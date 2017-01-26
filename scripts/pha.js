d16eb99bdd75de8cb0baf1dd89d8107363e041de
Evgeny Degtev <e.a.degtev@gmail.com>
2017-01-26 19:36:33 +0300

    feat: Export header javascript into separate file.

--- scripts/pha.js
+++ scripts/pha.js
@@ -1,205 +1,86 @@
-﻿// Variables for the animated header.
-var xOff = 0,
-yOff = 0,
-xInc = 1,
-yInc = 1,
-red = 0,
-green = 0,
-blue = 0,
-width = 1100, // ширина
-height = 80, // высота
-yMin = 0,
-iX = xInc,
-iY = yInc,
-slogan;
-//var count = 0;
+﻿function animateHeader() {
+  'use strict';
 
-function move() {
-  yOff += yInc;
-  xOff += 2 * xInc;
+  // Variables for the animated header.
+  var slogan = document.getElementById("slogan"),
+  xOff = 0, // offset
+  yOff = 0,
+  xInc = 1, // increment
+  yInc = 1,
+  width,
+  height;
 
-  if (yOff >= height) {
-    yInc = -1;
-  }
+  // Header dimensions detection for the purposes of animating the slogan.
+  function headerSizeDetect() {
+    var oldWidth, oldHeight;
 
-  if (yOff <= yMin) {
-    yInc = 1;
-  }
+    width = document.getElementById("header").clientWidth - slogan.clientWidth - 2;
+    height = document.getElementById("header").clientHeight - slogan.clientHeight;
 
-  if (xOff >= width) {
-    xInc = -1;
-  }
+    // This condition checks whether the actual sizes of the header are changed
+    // in some scenarios (zooming, orientation change etc.).
+    if (width == oldWidth && height == oldHeight) {
+      return;
 
-  if (xOff <= 0) {
-    xInc = 1;
-  }
-
-  slogan.style.top = yOff + "px";
-  slogan.style.left = xOff + "px";
-
-  /*if (count >= 50) {
-    red = Math.round(Math.random() * 255);
-    green = Math.round(Math.random() * 255);
-    blue = Math.round(Math.random() * 100 + 155);
-    slogan.color = MakeColor(red, green, blue);
-    count = 0;
-  }*/
-
-  if (iX != xInc) {
-    red = Math.round(Math.random() * 255);
-    green = Math.round(Math.random() * 255);
-    blue = Math.round(Math.random() * 255);
-    slogan.color = MakeColor(red, green, blue);
-    iX = xInc;
-  }
-
-  if (iY != yInc) {
-    red = Math.round(Math.random() * 255);
-    green = Math.round(Math.random() * 255);
-    blue = Math.round(Math.random() * 255);
-    slogan.color = MakeColor(red, green, blue);
-    iY = yInc;
-  }
-
-  window.requestAnimationFrame(move);
-  //count++;
-}
-//---------------------------------------------------------------
-function MakeColor(red, green, blue) {
-  red = red.toString(16);
-  green = green.toString(16);
-  blue = blue.toString(16);
-
-  if (red.length == 1) {
-    red = '0' + red;
-  }
-
-  if (green.length == 1) {
-    green = '0' + green;
-  }
-
-  if (blue.length == 1) {
-    blue = '0' + blue;
-  }
-  return "#" + red + green + blue;
-}
-
-//--------------------------------------------------------------------------------
-function showBrowVer() {
-
-  //вызываем функцию, параметр НЕ передаем,
-  //поэтому в результате получим все знаки версии после запятой
-  //alert("Браузер: "+data[0]); //выводим результат
-  var data = browserDetectNav();
-
-  if (data[0] == "Chrome") {
-    yOff = -10;
-    height = 70;
-    yMin =- 10;
-    width = 1110;
-  } else {
-
-    if (data[0] == "Safari") {
-      yOff =- 10;
-      height = 70;
-      yMin =- 10;
-      width = 1110;
     } else {
+      oldWidth = width;
+      oldHeight = height;
 
-      if (data[0] == "Firefox") {
-        yOff =- 10;
-        height = 75;
-        yMin =- 10;
-        width = 1140;
-      }
+      window.requestAnimationFrame(headerSizeDetect);
     }
   }
 
-  slogan = document.getElementById("slogan");
-  move();
-}
+  headerSizeDetect();
 
-//----------------------------------------------------------------------------------
-function browserDetectNav(chrAfterPoint) {
+  // This makes slogan drift.
+  function makeMove() {
+    yOff += yInc;
+    xOff += 2 * xInc;
 
-  // содержит переданный браузером юзерагент
-  var UA = window.navigator.userAgent,
+    // Checks whether the slogan reached the border of his parent node.
+    if (yOff >= height || yOff <= 0 || xOff >= width || xOff <= 0) {
+      slogan.style.color = makeColor();
 
-  //--------------------------------------------------------------------------------
-  // шаблоны для распарсивания юзерагента
-  OperaB = /Opera[ \/]+\w+\.\w+/i,
-  OperaV = /Version[ \/]+\w+\.\w+/i,
-  FirefoxB = /Firefox\/\w+\.\w+/i,
-  ChromeB = /Chrome\/\w+\.\w+/i,
-  SafariB = /Version\/\w+\.\w+/i,
-  IEB = /MSIE *\d+\.\w+/i,
-  SafariV = /Safari\/\w+\.\w+/i,
+      // Switching the increment.
+      (yOff == height || yOff == 0) ? yInc = -yInc : xInc = -xInc;
 
-  //--------------------------------------------------------------------------------
-  //массив с данными о браузере
-  browser = new Array(),
-
-  //шаблон для разбивки данных о браузере из строки
-  browserSplit = /[ \/\.]/i,
-  OperaV = UA.match(OperaV),
-  Firefox = UA.match(FirefoxB),
-  Chrome = UA.match(ChromeB),
-  Safari = UA.match(SafariB),
-  SafariV = UA.match(SafariV),
-  IE = UA.match(IEB),
-  Opera = UA.match(OperaB);
-
-  //----- Opera ----
-  if ((!Opera == "") & (!OperaV == "")) {
-    browser[0] = OperaV[0].replace(/Version/, "Opera");
-  } else {
-
-    if (!Opera == "") {
-      browser[0] = Opera[0];
-    } else {
-
-      //----- IE -----
-      if (!IE == "") {
-        browser[0] = IE[0];
-      } else {
-
-        //----- Firefox ----
-        if (!Firefox == "") {
-          browser[0] = Firefox[0];
-        } else {
-
-          //----- Chrome ----
-          if (!Chrome == "") {
-            browser[0] = Chrome[0];
-          } else {
-
-            //----- Safari ----
-            if ((!Safari == "") && (!SafariV == "")) {
-              browser[0] = Safari[0].replace("Version", "Safari");
-            }
-          }
-        }
+      // This catches the slogan overshooting when viewport is resizing.
+      if (yOff > height || yOff < 0 || xOff > width + 1 || xOff < 0) {
+        xOff = 2;
+        yOff = 1;
       }
     }
+
+    slogan.style.top = yOff + "px";
+    slogan.style.left = xOff + "px";
+
+    window.requestAnimationFrame(makeMove);
   }
 
-//------------ Разбивка версии -----------
-// возвращаемый функцией массив значений
-// [0] - имя браузера, [1] - целая часть версии
-// [2] - дробная часть версии
-  var outputData;
+  // This makes slogan color change.
+  function makeColor() {
+    var color,
+    finalColor = "#";
 
-  if (browser[0] != null) {
-    outputData = browser[0].split(browserSplit);
+    for (var i = 0; i < 3; i++) {
+      color = Math.round(Math.random() * 255);
+      color = color.toString(16);
+
+      if (color.length == 1) {
+        color = '0' + color;
+      }
+
+      finalColor = finalColor + color;
+    }
+
+    return finalColor;
   }
 
-  if ((chrAfterPoint == null) && (outputData != null)) {
-    chrAfterPoint = outputData[2].length;
+  window.requestAnimationFrame(makeMove);
 
-    // берем нужное ко-во знаков
-    outputData[2] = outputData[2].substring(0, chrAfterPoint);
-    return outputData;
+  window.addEventListener("resize", function() {
+    window.requestAnimationFrame(headerSizeDetect);
+  });
 
-  } else return false;
+  window.addEventListener('orientationchange', headerSizeDetect);
 }
-//-------------------------------------------------------------------------------------
